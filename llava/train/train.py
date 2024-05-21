bce8970f6fde2ece520962b8e69973c3e0d4e9b3
Daria Vorontsova <dariavvoroncova@gmail.com>
2024-05-21 23:49:47 +0300

    update

--- llava/train/train.py
+++ llava/train/train.py
@@ -502,6 +502,101 @@ def preprocess_v1(
         labels=targets,
     )
 
+def preprocess_protein(
+    sources,
+    tokenizer: transformers.PreTrainedTokenizer,
+    has_image: bool = False
+) -> Dict:
+    conv = conversation_lib.default_conversation.copy()
+    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+
+    # Apply prompt templates
+    conversations = []
+    conv.messages = []
+    START_ASK = "Describe the function of this protein: "
+    PROTEIN_EMBEDDING_TOKEN = "<protein>"  # todo
+    # подаём в ллм запрос с последовательностью (не эмбеддинги)
+    # query = f'{START_ASK}{PROTEIN_EMBEDDING_TOKEN}{sources["sequence"]}'
+    query = f'{START_ASK}'
+    conv.append_message(roles["human"], query)
+
+    functions = "\n".join(sources["protein_function"])
+    conv.append_message(roles["gpt"], functions)
+
+    conversations.append(conv.get_prompt())
+
+    targets = tokenizer(
+            functions,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+        ).input_ids
+
+
+    # Tokenize conversations todo
+    if has_image:
+        input_ids = torch.stack(
+            [tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+    else:
+        input_ids = tokenizer(
+            conversations,
+            return_tensors="pt",
+            padding="longest",
+            max_length=tokenizer.model_max_length,
+            truncation=True,
+        ).input_ids
+
+    # targets = input_ids.clone()
+
+    assert conv.sep_style == conversation_lib.SeparatorStyle.TWO
+
+    # Mask targets
+    sep = conv.sep + conv.roles[1] + ": "
+    for conversation, target in zip(conversations, targets):
+        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+
+        rounds = conversation.split(conv.sep2)
+        cur_len = 1
+        target[:cur_len] = IGNORE_INDEX
+        for i, rou in enumerate(rounds):
+            if rou == "":
+                break
+
+            parts = rou.split(sep)
+            if len(parts) != 2:
+                break
+            parts[0] += sep
+
+            if has_image:
+                round_len = len(tokenizer_image_token(rou, tokenizer))
+                instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 2
+            else:
+                round_len = len(tokenizer(rou).input_ids)
+                instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+
+            if i != 0 and not tokenizer.legacy and IS_TOKENIZER_GREATER_THAN_0_14:
+                round_len -= 1
+                instruction_len -= 1
+
+            target[cur_len: cur_len + instruction_len] = IGNORE_INDEX
+
+            cur_len += round_len
+        target[cur_len:] = IGNORE_INDEX
+
+        if cur_len < tokenizer.model_max_length:
+            if cur_len != total_len:
+                target[:] = IGNORE_INDEX
+                print(
+                    f"WARNING: tokenization mismatch: {cur_len} vs. {total_len}."
+                    f" (ignored)"
+                )
+
+    return dict(
+        input_ids=input_ids,
+        labels=targets,
+    )
+
 
 def preprocess_mpt(
     sources,
@@ -617,7 +712,8 @@ def preprocess_plain(
 def preprocess(
     sources: Sequence[str],
     tokenizer: transformers.PreTrainedTokenizer,
-    has_image: bool = False
+    has_image: bool = False,
+    is_protein: bool = False
 ) -> Dict:
     """
     Given a list of sources, each is a conversation list. This transform:
@@ -626,14 +722,18 @@ def preprocess(
     3. Tokenize the concatenated conversation;
     4. Make a deepcopy as the target. Mask human words with IGNORE_INDEX.
     """
-    if conversation_lib.default_conversation.sep_style == conversation_lib.SeparatorStyle.PLAIN:
-        return preprocess_plain(sources, tokenizer)
-    if conversation_lib.default_conversation.sep_style == conversation_lib.SeparatorStyle.LLAMA_2:
-        return preprocess_llama_2(sources, tokenizer, has_image=has_image)
-    if conversation_lib.default_conversation.version.startswith("v1"):
-        return preprocess_v1(sources, tokenizer, has_image=has_image)
-    if conversation_lib.default_conversation.version == "mpt":
-        return preprocess_mpt(sources, tokenizer, has_image=has_image)
+    if not is_protein:
+        if conversation_lib.default_conversation.sep_style == conversation_lib.SeparatorStyle.PLAIN:
+            return preprocess_plain(sources, tokenizer)
+        if conversation_lib.default_conversation.sep_style == conversation_lib.SeparatorStyle.LLAMA_2:
+            return preprocess_llama_2(sources, tokenizer, has_image=has_image)
+        if conversation_lib.default_conversation.version.startswith("v1"):
+            return preprocess_v1(sources, tokenizer, has_image=has_image)
+        if conversation_lib.default_conversation.version == "mpt":
+            return preprocess_mpt(sources, tokenizer, has_image=has_image)
+    else:
+        return preprocess_protein(sources, tokenizer)
+
     # add end signal and concatenate together
     conversations = []
     for source in sources:
@@ -731,6 +831,7 @@ class LazySupervisedDataset(Dataset):
                 self.data_args)
         else:
             sources = copy.deepcopy([e["conversations"] for e in sources])
+
         data_dict = preprocess(
             sources,
             self.tokenizer,
@@ -793,18 +894,24 @@ class ProteinDataset(Dataset):
         # assert len(sources) == 1, "Don't know why it is wrapped to a list"  # FIXME
         # todo туда нужно отправлять labels, но непонятно что это
         batch_labels, batch_strs, protein_tokens = self.batch_converter([(sources["id"], sources["sequence"])])
+        sources["protein_tokens"] = protein_tokens
 
-        for seq in sources:
-        input_ids = self.tokenizer(
-            sources["protein_function"],
-            return_tensors="pt",
-            padding="longest",
-            max_length=self.tokenizer.model_max_length,
-            truncation=True,
-        ).input_ids
+        data_dict_llm = preprocess(
+            sources,
+            self.tokenizer,
+            has_image=('image' in self.list_data_dict[i]), is_protein=True)
 
-        data_dict["input_ids"] = input_ids
+        # input_ids = self.tokenizer(
+        #     sources["protein_function"],
+        #     return_tensors="pt",
+        #     padding="longest",
+        #     max_length=self.tokenizer.model_max_length,
+        #     truncation=True,
+        # ).input_ids
+
+        data_dict["input_ids"] = data_dict_llm["input_ids"]
         data_dict['sequence'] = protein_tokens
+        data_dict["labels"] = data_dict_llm["labels"]
         return data_dict
 
 
@@ -839,6 +946,12 @@ class DataCollatorForSupervisedDataset(object):
             else:
                 batch['images'] = images
 
+        if 'image' in instances[0]:
+            images = [instance['image'] for instance in instances]
+            if all(x is not None and x.shape == images[0].shape for x in images):
+                batch['images'] = torch.stack(images)
+            else:
+                batch['images'] = images
         return batch
 
 
