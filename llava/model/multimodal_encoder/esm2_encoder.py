ffb6ab046273c36d2207e233d61e1575a270b01a
DariaVoo <dariavvoroncova@gmail.com>
2024-06-11 19:30:25 +0300

    get represintations of protein

--- llava/model/multimodal_encoder/esm2_encoder.py
+++ llava/model/multimodal_encoder/esm2_encoder.py
@@ -43,15 +43,9 @@ class ESMTower(nn.Module):
 
         self.is_loaded = True
 
-    def feature_select(self, image_forward_outs):
-        image_features = image_forward_outs.hidden_states[self.select_layer]
-        if self.select_feature == 'patch':
-            image_features = image_features[:, 1:]
-        elif self.select_feature == 'cls_patch':
-            image_features = image_features
-        else:
-            raise ValueError(f'Unexpected select feature: {self.select_feature}')
-        return image_features
+    def feature_select(self, protein_forward_outs):
+        protein_features = protein_forward_outs["representations"]
+        return protein_features
 
     @torch.no_grad()
     def forward(self, sequences):
